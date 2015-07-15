3747f8da0294383bc2345e8c6fc4b5ac8fa0183c
Yasoob <yasoob_rasheed@yahoo.com>
2015-07-15 17:48:35 -0500

    Cleared out Yasoob's comments

--- School Kit/GradeCalcViewController.swift
+++ School Kit/GradeCalcViewController.swift
@@ -16,16 +16,7 @@ class GradeCalcViewController: UIViewController, UITableViewDataSource, UITableV
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return calculators.count
@@ -49,6 +40,8 @@ class GradeCalcViewController: UIViewController, UITableViewDataSource, UITableV
         }
     }
     
-    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
