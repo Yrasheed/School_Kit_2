f56394ea5bb65c740d23bfeb66c76c86a02b1a19
Yasoob <yasoob_rasheed@yahoo.com>
2015-07-15 12:59:09 -0500

    Designed first view controller

--- School Kit/ViewController.swift
+++ School Kit/ViewController.swift
@@ -8,59 +8,58 @@
 
 import UIKit
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let newRed = CGFloat(red)/255
+        let newGreen = CGFloat(green)/255
+        let newBlue = CGFloat(blue)/255
+        
+        self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 0.9)
+    }
+}
+
 class ViewController: UIViewController {
     
-//    var block1 = UIView()
-//    
-//    var animator : UIDynamicAnimator? = nil
-//    let gravity = UIGravityBehavior()
-//    let collider = UICollisionBehavior()
-    
+    @IBOutlet weak var getStartedButton: UIButton!
+
     var animator : UIDynamicAnimator!
     var gravity : UIGravityBehavior!
+    var cgWhite : CGColor!
     
-//    func createAnimatorStuff() {
-//        animator = UIDynamicAnimator(referenceView: self.view)
-//        animator?.addBehavior(collider)
-//        collider.addItem(block1)
-//        //collider.translatesReferenceBoundsIntoBoundary = true
-//
-//        gravity.addItem(block1)
-//        gravity.gravityDirection = CGVectorMake(0, 1)
-//        animator?.addBehavior(gravity)
-//    }
-//    
-//    
-//    func addBox(location: CGRect) {
-//        let newBlock = UIView(frame: location)
-//        newBlock.backgroundColor = UIColor.redColor()
-//        view.insertSubview(newBlock, atIndex: 0)
-//        block1 = newBlock
-//    }
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.barrier.removeFromSuperview()
-//        addBox(CGRectMake(0, -700, 380, 668))
-//        createAnimatorStuff()
-//        let block = UIView(frame: CGRect(x: 0, y: -700, width: 380, height: 668))
-//        block.backgroundColor = UIColor.magentaColor()
-//        view.addSubview(block)
-//        animator = UIDynamicAnimator(referenceView: view)
-//        gravity = UIGravityBehavior(items: [block])
-//        gravity.gravityDirection = CGVectorMake(0, 2)
-//        animator.addBehavior(gravity)
-//        let collision = UICollisionBehavior(items: [block, barrier])
-//        collision.collisionMode = .Everything
-//        collision.addBoundaryWithIdentifier("bottomCollision", fromPoint: CGPointMake(0, self.view.frame.height + 200), toPoint: CGPointMake(self.view.frame.width, self.view.frame.height + 200))
-//        collision.translatesReferenceBoundsIntoBoundary = false
-//        animator.addBehavior(collision)
         
+        getStartedButton.layer.borderColor = CGColorCreate(CGColorSpaceCreateDeviceRGB(), [1.0, 1.0, 1.0, 1.0])
+        getStartedButton.layer.borderWidth = CGFloat(2)
+        getStartedButton.layer.cornerRadius = 30
+        
+        let block = UIView(frame: CGRect(x: 0, y: -700, width: 380, height: 668))
+        let greenSwiftColor = UIColor(red: 160, green: 235, blue: 75)
+        block.backgroundColor = greenSwiftColor
+        view.addSubview(block)
+        view.sendSubviewToBack(block)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [block])
+        gravity.gravityDirection = CGVectorMake(0, 2)
+        animator.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: [block])
+        collision.collisionMode = .Everything
+        collision.addBoundaryWithIdentifier("bottomCollision", fromPoint: CGPointMake(0, self.view.frame.height + 2), toPoint: CGPointMake(self.view.frame.width, self.view.frame.height + 2))
+        animator.addBehavior(collision)
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        //borderwidth
+        //bordercolor
+        //green r188 g255 b121
+        //blue r179 g242 b252
+        //yellow r242 g242 b99
         // Dispose of any resources that can be recreated.
     }
 
